a3764166aebe84daed087fb1d996b962e6e22ef0
Nyanibah Escoffery <naomi@diamondrecovery.net>
2023-09-20 03:33:06 -0500

    done

---
+++ index.js
@@ -0,0 +1,41 @@
+function shout(string) {
+    // todo
+  }
+  function shout(string) {
+    return string.toUpperCase();
+  }
+  function whisper(string) {
+    // todo
+  }
+  function whisper(string) {
+    return string.toLowerCase();
+  }
+  function logShout(string) {
+    console.log(string.toUpperCase())
+  }
+  function logWhisper(string) {
+    console.log(string.toLowerCase())
+  }
+  function sayHiToHeadphonedRoommate(string) {
+    // to do
+    if (string === string.toLowerCase()) {
+        // Block of code
+        return("I can't hear you!")
+      }
+      if (string === string.toUpperCase()) {
+        //Block of Code
+        return("YES INDEED!")
+      }
+      if (string === "Let's have dinner together!") {
+        //Block of Code
+        return("I would love to!")
+      }
+      
+  }
+
+  const string = "i can\'t hear you!";
+
+console.log(string);
+console.log(string.toLowerCase());
+
+console.log(string === string.toLowerCase());
